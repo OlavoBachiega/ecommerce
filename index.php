35b76ebba41eefa8d25bef5da8652e1602e157b5
OlavoBachiega <olavobachiega@yahoo.com.br>
2017-10-29 15:55:01 -0200

    tela de login

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -12,17 +13,48 @@ $app->config('debug', true);
 
 $app->get('/', function() {
 
-  $page = new Page();
+ 	$page = new Page();
 
-  $page->setTpl("index");
+  	$page->setTpl("index");
 
 });
 
 $app->get('/admin', function() {
 
-  $page = new PageAdmin();
+	User::verifyLogin();
 
-  $page->setTpl("index");
+  	$page = new PageAdmin();
+
+  	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+
+  	$page = new PageAdmin([
+  		"reader"=>false,
+  		"footer"=>false
+  	]);
+
+  	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
