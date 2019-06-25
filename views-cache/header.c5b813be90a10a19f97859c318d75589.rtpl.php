1097e6cfd730aaa59c9d99b1996fecbab85207e9
OlavoBachiega <olavobachiega@yahoo.com.br>
2019-06-25 15:14:21 -0300

    Finalizar Pedido - Boleto

--- views-cache/header.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/header.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -57,7 +57,7 @@
                     <div class="header-right">
                         <ul class="list-unstyled list-inline">
                             <li class="dropdown dropdown-small">
-                                <a data-toggle="dropdown" data-hover="dropdown" class="dropdown-toggle" href="#"><span class="key">moeda: </span><span class="value">BRL </span><b class="caret"></b></a>
+                                <a data-toggle="dropdown" data-hover="dropdown" class="dropdown-toggle" href="#"><span class="key">moeda :</span><span class="value">BRL </span><b class="caret"></b></a>
                                 <ul class="dropdown-menu">
                                     <li><a href="#">BRL</a></li>
                                     <li><a href="#">USD</a></li>
@@ -65,7 +65,7 @@
                             </li>
 
                             <li class="dropdown dropdown-small">
-                                <a data-toggle="dropdown" data-hover="dropdown" class="dropdown-toggle" href="#"><span class="key">linguagem: </span><span class="value">Português </span><b class="caret"></b></a>
+                                <a data-toggle="dropdown" data-hover="dropdown" class="dropdown-toggle" href="#"><span class="key">linguagem :</span><span class="value">Português </span><b class="caret"></b></a>
                                 <ul class="dropdown-menu">
                                     <li><a href="#">Português</a></li>
                                     <li><a href="#">Inglês</a></li>
